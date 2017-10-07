40ff65e657c0bb1e85978beaae202c28e9e7608d
Andy Damevin <andy.damevin@neotys.com>
2017-10-07 18:13:17 +0200

    AddDependencyCommand tested

--- src/main/java/org/ia3andy/forge/addon/java/guava/ui/AbstractGuavaCommand.java
+++ src/main/java/org/ia3andy/forge/addon/java/guava/ui/AbstractGuavaCommand.java
@@ -3,6 +3,8 @@ package org.ia3andy.forge.addon.java.guava.ui;
 import javax.inject.Inject;
 
 import org.jboss.forge.addon.facets.FacetFactory;
+import org.jboss.forge.addon.facets.constraints.FacetConstraint;
+import org.jboss.forge.addon.maven.projects.MavenFacet;
 import org.jboss.forge.addon.projects.ProjectFactory;
 import org.jboss.forge.addon.projects.ui.AbstractProjectCommand;
 import org.jboss.forge.addon.ui.context.UIContext;
@@ -12,6 +14,7 @@ import org.jboss.forge.addon.ui.result.Results;
 import org.jboss.forge.addon.ui.util.Categories;
 import org.jboss.forge.addon.ui.util.Metadata;
 
+@FacetConstraint({ MavenFacet.class })
 public abstract class AbstractGuavaCommand extends AbstractProjectCommand {
 
     private static final String GUAVA_CATEGORY = "Guava";
--- src/main/java/org/ia3andy/forge/addon/java/guava/ui/GuavaAddDependencyCommand.java
+++ src/main/java/org/ia3andy/forge/addon/java/guava/ui/GuavaAddDependencyCommand.java
@@ -3,13 +3,12 @@ package org.ia3andy.forge.addon.java.guava.ui;
 import java.util.Optional;
 
 import org.ia3andy.forge.addon.java.guava.facet.GuavaFacet;
-import org.jboss.forge.addon.facets.constraints.FacetConstraint;
 import org.jboss.forge.addon.projects.Project;
 import org.jboss.forge.addon.ui.context.UIBuilder;
 import org.jboss.forge.addon.ui.context.UIExecutionContext;
 import org.jboss.forge.addon.ui.result.Result;
 
-@FacetConstraint({ GuavaFacet.class })
+
 public class GuavaAddDependencyCommand extends AbstractGuavaCommand {
 
 	@Override
